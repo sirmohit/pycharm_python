df730edf1df15b97713c45f6d225f10afc2d5455
Mohit Bhade <128077306+sirmohit@users.noreply.github.com>
2024-02-04 21:28:25 +0530

    Add files via upload

---
+++ f_string.py
@@ -0,0 +1,6 @@
+# f-string ; String which is prefix with f
+name = "Mohit"
+age = 23
+height = 5.5
+print("My name is " + name,"I am "+ str(age) ,"years old", "My height is ",height )
+print(f"My name is {name} I am {age} old My height is {height} feet.")
