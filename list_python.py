31624d253ce2447888406b0e178808d1f962930b
Mohit Bhade <128077306+sirmohit@users.noreply.github.com>
2024-02-10 14:00:37 +0530

    Add files via upload

---
+++ list_python.py
@@ -0,0 +1,35 @@
+'''numbers=[12,23,34,4332,125,32]
+print(numbers)
+names = ["mohit","ankit","deepak","sourav","khushi"]
+print(names)
+names.sort()
+print(names)
+mix_list= [1,"Khushi",12.34,23+3j,True]
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+numbers.append(11)
+print(numbers)
+numbers.count(4332)
+print(numbers)
+numbers.pop()
+print(numbers)
+numbers.remove(32)
+print(numbers)
+numbers.insert(1,3)
+print(numbers)
+numbers.extend(names)
+print(numbers)
+numbers.clear()
+print(numbers)'''
+
+# Nested list
+'''
+list1 =[23,43,54,423,[34,54,65,76],43,32]
+print(len(list1))
+print(list1[4][3])
+print(list1[4][:])
+print(list1[4][1:3])
+print(list1[4][0:3:2])
+print(list1[4][-1:-4])'''
