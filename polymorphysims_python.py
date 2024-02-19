80d0882ed1311880863a40879e1b6e9cd35942ce
Mohit Bhade <128077306+sirmohit@users.noreply.github.com>
2024-02-19 13:50:35 +0530

    Add files via upload

---
+++ polymorphysims_python.py
@@ -0,0 +1,6 @@
+# POLTMORPHYSIM MEANS MANY FORMS
+# In Python we use to achieve the polymorphism through 4 ways:
+#1 Duck Typing
+#2 Method Overloading
+#3 Operator Overloading
+#4 Method Overriding
